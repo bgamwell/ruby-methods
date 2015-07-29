980547cad29813ce8b5ef5d296253977040682c9
bgamwell <bgamwell@gmail.com>
2015-07-29 16:12:20 -0700

    Practicing ruby

--- ruby-classes.rb
+++ ruby-classes.rb
@@ -1,12 +1,12 @@
 class Monster
 
-  attr_accessor :habitat
+  attr_accessor :habitat, :threat_level
+
   @@count = 0
 
   # default methods for whenever a new instance of monster is initialized
   def initialize
     puts "Rawr!"
-    @threat_level = "Meh"
     @@count = @@count + 1
   end
 
@@ -24,6 +24,10 @@ class Monster
     puts true if @habitat == "Under the bed"
   end
 
+  def fight(monster1, monster2)
+     puts "Monster 1 is more dangerous!" if monster1.threat_level == "high" && monster2.threat_level == "low"
+  end
+
   # # Setter method for habitat
   # def habitat=(habitat)
   #   @habitat = habitat
@@ -37,7 +41,12 @@ class Monster
 end
 
 monster = Monster.new
+monster2 = Monster.new
 monster.habitat = ("Under the bed")
 monster.habitat?
+monster.threat_level = ("high")
+monster2.threat_level = ("low")
 p Monster.count
 p monster
+p monster2
+p monster.fight(monster,monster2)
